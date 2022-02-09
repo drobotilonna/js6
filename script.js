248876006eb43ce9d400faee59c7d1201c687405
Ilona Drobot <drobotilonna@gmail.com>
2022-02-09 18:05:33 +0200

    js8

---
+++ script.js
@@ -0,0 +1,19 @@
+let person = {
+    name: "Ilona",
+    age: 13,
+    city: "Kiev",
+    address: {
+        srteet: "Лесі Українки", 
+        building: 2,
+        apt:584
+
+    },
+    f:function (text) {
+        console.log(text)
+    },
+    
+}
+
+person.f("ok")
+console.log(person)
+
